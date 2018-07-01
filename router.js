9ca6dc513aaf50bfb4fd42d14dd2561d8a8d9732
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2018-07-01 22:26:30 +0530

    dynamic templating for routers. handles dynamic variable bindings in templates.

--- router.js
+++ router.js
@@ -18,10 +18,32 @@ router = () => {
     if (view) {
         let currRoute = getCurrentRoute();
         if (currRoute && routes[currRoute] && routes[currRoute].controller) {
-            view.innerHTML = document.getElementById(routes[currRoute].template).innerHTML;
+            view.innerHTML = renderTemplate(document.getElementById(routes[currRoute].template).innerHTML, new routes[currRoute].controller);
         }
     }
 };
 
+renderTemplate = (html, controller) => {
+    if (html && html.indexOf('{{') !== -1) {
+        let cache = {};
+        var reg = /{{(.*?)}}/g;
+        while (s = reg.exec(html)) {
+            if (controller[s[1]]) {
+                if (!cache[s[0]]) {
+                    cache[s[0]] = {
+                        use: controller[s[1]]
+                    };
+                }
+            }
+        }
+        for (let i in cache) {
+            html = html.split(i).join(cache[i].use);
+        }
+    }
+    return html;
+};
+
 window.addEventListener('hashchange', router);
-window.addEventListener('load', router);
+window.addEventListener('load', function() {
+    setTimeout(router, 2000);
+});
