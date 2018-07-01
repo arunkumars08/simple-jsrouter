dfac8884e185b7fa949d469b389e3e79875967b9
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2018-07-01 22:26:30 +0530

    dynamic templating for routers. handles dynamic variable bindings in templates.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,23 @@ init = () => {
 };
 
 registerRoutes = () => {
-    route('home', 'homeTmp', function() {});
-    route('about-us', 'aboutTmp', function() {});
-    route('contact-us', 'contactTmp', function() {});
+    route('home', 'homeTmp', function() {
+        this.pageName = 'Home!';
+        this.aboutUsRouteLink = 'about-us';
+        this.aboutUsRouteName = 'About Us';
+        this.contactUsRouteLink = 'contact-us';
+        this.contactUsRouteName = 'Contact Us';
+    });
+    route('about-us', 'aboutTmp', function() {
+        this.pageName = 'About Us!';
+        this.homeRouteLink = 'home';
+        this.homeRouteName = 'Home';
+    });
+    route('contact-us', 'contactTmp', function() {
+        this.pageName = 'Contact Us!';
+        this.homeRouteLink = 'home';
+        this.homeRouteName = 'Home';
+    });
 };
 
 window.addEventListener('load', init);
